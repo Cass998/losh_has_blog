3c8f14a6c9fb083178224a62ad19d21809b5f059
Cass998 <45821602+Cass998@users.noreply.github.com>
2026-07-14 20:59:15 +0800

    Expand veRL architecture and Ray guide

---
+++ public/verl-learning/assets/chunks/swimlanesDiagram-G3AALYLV.MRWEgVyl.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.qSXbl7j4.js";import{_ as a}from"../app.DoAJQegv.js";import"./chunk-5VM5RSS4.HwwCdp4W.js";import"./chunk-XXDRQBXY.twp_dlS_.js";import"./chunk-VR4S4FIN.DaMjwABF.js";import"./chunk-32BRIVSS.CswDSd2Z.js";import"./channel.qPttU8Rq.js";import"./framework.C49Ar5GK.js";import"./theme.xDhdzmbr.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
